b9354e0326c8cb5159c9089a60b8b0431e9fcc4b
Matt Dudzik <mdudzik92@gmail.com>
2021-07-26 23:34:34 -0500

    Added playing class for transition on key click, added event listener to end the transition after 0.07s

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 window.addEventListener("keydown", function (e) {
 	// Instantiating audio by grabbing the keyCode of the keys that have sound srcs with attribute selector
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	// Stop the function from running all together if there's no audio associated with that key
 	if (!audio) return;
 	// Rewind audio file to start so it allows hitting more than once before audio file actually ends
 	audio.currentTime = 0;
 	audio.play();
+	key.classList.add("playing");
 });
+
+function removeTransition(e) {
+	// Skip it if it's not a transform
+	if (e.propertyName !== "transform") return;
+	this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
