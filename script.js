7e97c6d3caafbbb95bfae6d8f2d824bfee0548e9
Matt Dudzik <mdudzik92@gmail.com>
2021-07-26 23:19:58 -0500

    Setting current audio time to 0 so we can hit more than one sound at a time

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 window.addEventListener("keydown", function (e) {
-	// Instantiate audio
+	// Instantiating audio by grabbing the keyCode of the keys that have sound srcs with attribute selector
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
 	// Stop the function from running all together if there's no audio associated with that key
 	if (!audio) return;
+	// Rewind audio file to start so it allows hitting more than once before audio file actually ends
+	audio.currentTime = 0;
 	audio.play();
 });
